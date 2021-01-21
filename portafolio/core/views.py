63c04f3d915c118726e1c4c9c97e69bf093c5b94
Ricardo Juarez <juarezqricardo@gmail.com>
2021-01-21 01:39:43 -0500

    Front end and back end joined

--- portafolio/core/views.py
+++ portafolio/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, "core/index.html")
+
+def aboutMe(request):
+    return render(request, "core/about.html")
+
+def portafolio(request):
+    return render(request, "core/portafolio.html")
+
+def contactMe(request):
+    return render(request, "core/contact.html")
--- portafolio/portafolio/urls.py
+++ portafolio/portafolio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core import views
 urlpatterns = [
+    path('', views.home, name="index"),
+    path('about-me/', views.aboutMe, name="about"),
+    path("portafolio/", views.portafolio, name = "portafolio"),
+    path("contact/", views.contactMe, name="contact"),
     path('admin/', admin.site.urls),
 ]
